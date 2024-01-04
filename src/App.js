d01eaa57acb2e22eab992a8a74349a97ab54788e
bharathzt <bharath.hy@zeksta.com>
2024-01-04 10:51:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/navbar/Navbar";
+import Home from "./components/home/Home";
+import { Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import Product from "./components/products/Product";
+import Footer from "./components/footer/Footer";
+import ProductCreate from "./components/productCreate/ProductCreate";
+import ProductEdit from "./components/productEdit/ProductEdit";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Product />} />
+        <Route path="/create" element={<ProductCreate />} />
+        <Route path="/edit" element={<ProductEdit />} />
+        <Route index path="*" element={<p> not found</p>} />
+      </Routes>
+      {/* <Footer /> */}
     </div>
   );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./footer.css"
+
+function Footer() {
+    return (
+        <footer>
+            <div className="footer-content">
+                <p>Copyright &Copy <span id="year">2023</span> </p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
